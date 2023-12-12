d72a30101380a95d48c7f334a5eac66b4094b545
simon-albrecht <simon.albrecht@posteo.de>
2023-12-12 13:43:43 +0100

    added pw and renamed app

--- app.py
+++ app.py
@@ -5,15 +5,56 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import streamlit as st
 import openai
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+
+
 #from streamlit_chat import message
 
 #from dotenv import load_dotenv, find_dotenv
 #load_dotenv(find_dotenv(), override=True)
 headers = {
-    "authorization":st.secrets['OPENAI_API_KEY'],
+    "authorization":st.secrets['OPENAIAPIKEY'],
     "content-type":"application/json"
     }
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAIAPIKEY"]
 
 st.title('Gaia chatbot')
 question = st.text_input("Write a question about GAIA: ", key="input")
